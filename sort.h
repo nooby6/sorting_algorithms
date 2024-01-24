53ab6ec6f01c323cc98e5b2b1274e82355fc3347
nooby6 <giftaudrine1@gmail.com>
2024-01-24 00:58:17 -0800

    header file

--- sort.h
+++ sort.h
@@ -37,6 +37,11 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+/* Function declarations */
+void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+void print_list(const listint_t *list);
+
 /* Sorting algorithms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
