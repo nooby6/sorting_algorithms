8f21b58f87ae3e99fbb007a91770b33fe6df7ad3
nooby6 <giftaudrine1@gmail.com>
2024-01-24 04:45:57 -0800

    Big O Notations

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,6 +3,7 @@
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers
  *                       in ascending order using the Insertion sort algorithm.
+ *
  * @list: A pointer to the head of the doubly linked list.
  */
 void insertion_sort_list(listint_t **list)
@@ -10,16 +11,17 @@ void insertion_sort_list(listint_t **list)
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	listint_t *current, *temp;
+	listint_t *current = (*list)->next, *temp;
 
-	current = (*list)->next;
 	while (current != NULL)
 	{
 		temp = current;
+
 		while (temp->prev != NULL && temp->n < temp->prev->n)
 		{
 			/* Swap the nodes */
 			temp->prev->next = temp->next;
+
 			if (temp->next != NULL)
 				temp->next->prev = temp->prev;
 
@@ -34,6 +36,7 @@ void insertion_sort_list(listint_t **list)
 
 			print_list(*list);  /* Print the list after each swap */
 		}
+
 		current = current->next;
 	}
 }
